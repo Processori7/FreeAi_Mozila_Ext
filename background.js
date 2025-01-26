625a2c1c4730a545d5af95159c2193877ef5da5b
Igor Usenko <usenkoigor01@gmail.com>
2025-01-26 15:19:59 +0300

    Исправление проблемы с открытием списка в боковой панели

--- background.js
+++ background.js
@@ -1,3 +1,14 @@
 browser.browserAction.onClicked.addListener(() => {
-    browser.sidebarAction.toggle();
-  });
+  // Всегда обновляем панель перед открытием
+  browser.sidebarAction.setPanel({ panel: "popup.html" });
+  
+  // Открываем sidebar без проверки состояния
+  browser.sidebarAction.open();
+});
+
+// Обновление содержимого sidebar из popup.js
+browser.runtime.onMessage.addListener((message) => {
+  if (message.action === "updateSidebar") {
+    browser.sidebarAction.setPanel({ panel: message.url });
+  }
+});
